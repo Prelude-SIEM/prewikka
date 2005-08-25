605f5905174144b0ebd86b1088da5a8999406186
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-08-25 02:01:51 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ class my_install(install):
 
 
 setup(name="prewikka",
-      version="0.9.0-rc10",
+      version="0.9.0-rc11",
       packages=[ 'prewikka', 'prewikka.views', 'prewikka.templates',
                  'prewikka.modules',
                  'prewikka.modules.log', 'prewikka.modules.log.stderr',
