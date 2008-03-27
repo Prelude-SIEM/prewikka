2c03de7b80b6c27e5f3cb8eae670446a1df9320a
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-03-27 09:17:57 +0000

    Update NEWS, bump version number.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from distutils.core import Command
 from Cheetah.CheetahWrapper import CheetahWrapper
 
 
-PREWIKKA_VERSION = "0.9.13"
+PREWIKKA_VERSION = "0.9.14"
 
 from fnmatch import fnmatch
 from distutils.dep_util import newer
