4208472bb6b81edded18aee9437f2addd4f2cf23
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2009-06-30 10:42:56 +0200

    Update NEWS, bump version number.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from distutils.core import Command
 from Cheetah.CheetahWrapper import CheetahWrapper
 
 
-PREWIKKA_VERSION = "0.9.15"
+PREWIKKA_VERSION = "0.9.16"
 LIBPRELUDE_REQUIRED_VERSION = "0.9.23"
 LIBPRELUDEDB_REQUIRED_VERSION = "0.9.12"
 
