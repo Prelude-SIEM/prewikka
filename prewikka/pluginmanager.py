Prelude-SIEM/prewikka

commit 1eedcbf790dab64253713167c5a9795de912e283

[#862] Better handling of sections in the menubar
- Configured sections that do not exist appear as menus
- Configured sections that have been loaded appear enabled in the previous menu
- Configured sections that fail to load appear disabled in the previous menu
- Non-configured sections that have been loaded appear in the ADMIN menu
- Non-configured sections that fail to load do not appear at all

Change-Id: I0e6ed79cfc2fadc02c13d76dde3f6da0f1c8a15e

prewikka/pluginmanager.py
@@ -136,6 +136,11 @@ class PluginManager:
         else:
             dh.check()
 
+    @staticmethod
+    def _handle_section(plugin_class):
+        if hasattr(plugin_class, "view_section") and plugin_class.view_section:
+            env.menumanager.add_section(plugin_class.view_section)
+
     def _addPlugin(self, plugin_class, autoupdate, name=None):
         self._handle_attributes(plugin_class, autoupdate)
 
@@ -170,9 +175,11 @@ class PluginManager:
 
             try:
                 if issubclass(plugin_class, PluginPreload):
+                    [self._handle_section(x) for x in plugin_class.plugin_classes]
                     self._handle_attributes(plugin_class, autoupdate)
-                    [self._addPlugin(x, autoupdate) for x in plugin_class().plugin_classes]
+                    [self._addPlugin(x, autoupdate) for x in plugin_class.plugin_classes]
                 else:
+                    self._handle_section(plugin_class)
                     self._addPlugin(plugin_class, autoupdate, name=name)
 
             except error.PrewikkaUserError as e:

prewikka/main.py
@@ -32,7 +32,7 @@ except ImportError:
 
 from prewikka import view, config, log, database, idmefdatabase, version, \
                      auth, error, utils, localization, resolve, theme, \
-                     pluginmanager, renderer, env, dataprovider
+                     pluginmanager, renderer, env, dataprovider, menu
 from prewikka.myconfigparser import ConfigParserSection
 
 from prewikka.templates import ClassicLayout
@@ -119,7 +119,8 @@ class BaseView(view._View):
 
         dataset["interface.active_tab"] = active_tab
         dataset["interface.active_section"] = active_section
-        dataset["interface.sections"] = env.viewmanager.getSections(user) if env.viewmanager else {}
+        dataset["interface.sections"] = env.menumanager.get_sections(user) if env.menumanager else {}
+        dataset["interface.menu"] = env.menumanager.get_menus(user) if env.menumanager else {}
         dataset["toplayout_extra_content"] = ""
 
         all(env.hookmgr.trigger("HOOK_TOPLAYOUT_EXTRA_CONTENT",
@@ -185,6 +186,7 @@ class Core:
         resolve.init()
 
         env.viewmanager = None
+        env.menumanager = None
         env.htdocs_mapping.update((("prewikka", pkg_resources.resource_filename(__name__, 'htdocs')),))
 
         try:
@@ -233,6 +235,7 @@ class Core:
         obj.init(config)
 
     def _loadPlugins(self, last_change=None):
+        env.menumanager = menu.MenuManager()
         env.viewmanager = view.ViewManager()
 
         env.plugins = {}

prewikka/menu.py
@@ -1,0 +1,118 @@
+# Copyright (C) 2016 CS-SI. All Rights Reserved.
+# Author: Antoine Luong <antoine.luong@c-s.fr>
+#
+# This file is part of the Prewikka program.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from copy import copy
+
+from prewikka import env, utils
+
+
+class MenuManager(object):
+    """
+    Handle section placement in the menus.
+    """
+    _DEFAULT_MENU = "ADMIN"
+    _DEFAULT_ICON = "sliders"
+
+    def __init__(self):
+        self._all_sections = set()
+        self._loaded_sections = {}
+        self._sections_path = {}
+        env.hookmgr.declare("HOOK_MENU_LOAD", multi=True)
+
+    def get_sections(self, user=None):
+        def _merge(d1, d2):
+            for section, tabs in d2.items():
+                d1[section] = copy(d1.get(section, {}))
+                for tab, views in tabs.items():
+                    d1[section][tab] = views
+
+        d = copy(self._loaded_sections)
+        [_merge(d, i) for i in env.hookmgr.trigger("HOOK_MENU_LOAD", user) if i]
+
+        return d
+
+    def get_sections_path(self):
+        return self._sections_path
+
+    def add_section(self, section):
+        self._all_sections.add(section)
+
+    def add_section_info(self, view):
+        self._loaded_sections.setdefault(view.view_section, utils.OrderedDict()) \
+                             .setdefault(view.view_name, utils.OrderedDict())[view.view_id] = view
+
+        self._sections_path.setdefault(utils.nameToPath(view.view_section), {}) \
+                           .setdefault(utils.nameToPath(view.view_name), utils.OrderedDict())[utils.nameToPath(view.view_id)] = view.view_path
+
+    def get_menus(self, user):
+        """
+        Return the menu structure in the following form:
+        {
+            menu1: {
+                icon: icon1,
+                entries: [
+                    {name: name11, link: link11, icon: icon11},
+                    {name: name12, link: link12, icon: icon12}
+                ]
+            },
+            menu2: {
+                icon: icon2,
+                entries: [
+                    ...
+                ]
+            }
+        }
+        """
+        loaded_sections = self.get_sections(user)
+        menus = utils.OrderedDict()
+        default_menu = {'icon': self._DEFAULT_ICON, 'entries': [], 'default': True}
+
+        for section, icon in env.config.section_order.items():
+            if section in self._all_sections:
+                # Sections that are declared in section_order but not loaded
+                # should appear in the menu, but with an empty link
+                link = self._get_first_view_path(loaded_sections.get(section))
+
+                # Put the section in the previous menu (or in the default one if there is none)
+                current_menu = menus.values()[-1] if menus else default_menu
+                current_menu["entries"].append({'name': section, 'link': link, 'icon': icon.value})
+
+            else:
+                # Create a new menu if the section_order entry does not match a section
+                is_default = section == self._DEFAULT_MENU
+                menus[section] = {'icon': icon.value, 'entries': [], 'default': is_default}
+
+        for section in loaded_sections:
+            # Put the sections not declared in section_order in the default menu
+            if section not in env.config.section_order:
+                link = self._get_first_view_path(loaded_sections.get(section))
+                default_menu["entries"].append({'name': section, 'link': link, 'icon': None})
+
+        if self._DEFAULT_MENU in menus:
+            menus[self._DEFAULT_MENU]["entries"] += default_menu["entries"]
+        else:
+            menus[self._DEFAULT_MENU] = default_menu
+
+        return menus
+
+    @staticmethod
+    def _get_first_view_path(section):
+        if not section:
+            return None
+        return section.values()[0].values()[0].view_path

prewikka/templates/TopLayout.tmpl
@@ -1,5 +1,4 @@
 #extends prewikka.templates.HTMLDocument
-#from prewikka import env
 
 #def body_class()
 #end def
@@ -70,46 +69,46 @@
             <div class="collapse navbar-collapse" id="main_navbar_collapse">
             #if $prewikka.user_display
                 <ul id="top_view_navbar_menu" class="nav navbar-nav navbar-primary">
-                #for $index, ($section, $tabs) in $enumerate($interface.sections.items()):
-                    #if not $tabs
-                        #if $index != 0
-                        </ul>
-                    </li>
-                        #end if
-                        #if $index != len($interface.sections) - 1
+                #for $name, $menu_item in $interface.menu.items()
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown">
-                            #if $env.config.section_order.get($section)
-                                <i class="fa fa-$env.config.section_order[$section].value"></i>
+                            #if $menu_item.icon
+                                <i class="fa fa-$menu_item.icon"></i>
                             #end if
-                            $_($section)
+                            $_($name)
                             <span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu" role="menu">
-                        #end if
-                    #else
-                            <li>
-                                <a href="${tabs.values()[0].values()[0].view_path}">
-                                #if $env.config.section_order.get($section)
-                                    <i class="fa fa-$env.config.section_order[$section].value"></i>
+                        #for $section in $menu_item.entries
+                            #if $section.link
+                            <li><a href="$section.link">
+                                #if $section.icon
+                                <i class="fa fa-$section.icon"></i>
                                 #end if
-                                $_($section)
-                                </a>
-                            </li>
-                        #if $index == len($interface.sections) - 1
+                                $_($section.name)
+                            </a></li>
+                            #else
+                            <li class="disabled" title="$_('This app has been disabled or failed to load.')"><a>
+                                #if $section.icon
+                                <i class="fa fa-$section.icon"></i>
+                                #end if
+                                $_($section.name)
+                            </a></li>
+                            #end if
+                        #end for
+                        #if $menu_item.default
                             <li role="separator" class="divider"></li>
                             <li><a class="widget-link" title="$_("About")" href="$prewikka.about">$_("About")</a></li>
                             #if $prewikka.logout_link
                             <li><a title="$_("Logout")" href="$prewikka.logout_link" data-confirm="$_("Are you sure you want to Logout ?")">$_("Logout")</a></li>
                             #end if
+                        #end if
                         </ul>
                     </li>
-                        #end if
-                    #end if
                 #end for
+                </ul>
             #end if
 #end filter
-                </ul>
                 <ul class="nav navbar-nav navbar-right" id="nav_top_view_header">
                     <li class="visible-lg-inline">
                         <div class="navbar-brand">$prewikka.software</div>

prewikka/view.py
@@ -394,23 +394,11 @@ class View(_View, pluginmanager.PluginBase):
 
 
 class ViewManager:
-    def getSections(self, user=None):
-        def _merge(d1, d2):
-                for section, tabs in d2.items():
-                        d1[section] = copy(d1.get(section, {}))
-                        for tab, views in tabs.items():
-                                d1[section][tab] = views
-
-        d = copy(self._sections)
-        [_merge(d, i) for i in env.hookmgr.trigger("HOOK_MENU_LOAD", user) if i]
-
-        return d
-
     def getViewPath(self, view_id, default=_sentinel):
         return getViewPath(view_id, default)
 
     def getViewID(self, request):
-        sections = self._sections_path
+        sections = env.menumanager.get_sections_path()
         paths = request.getViewElements()
 
         view_id = None
@@ -461,19 +449,12 @@ class ViewManager:
 
     def addView(self, view):
         if view.view_name:
-            self._addSectionInfo(view)
+            env.menumanager.add_section_info(view)
 
         self._views[view.view_id] = view
 
         env.hookmgr.declare("HOOK_%s_PARAMETERS_REGISTER" % view.view_id.upper())
         env.hookmgr.declare("HOOK_%s_PARAMETERS_NORMALIZE" % view.view_id.upper())
-
-    def _addSectionInfo(self, view):
-        self._sections.setdefault(view.view_section, utils.OrderedDict()) \
-                      .setdefault(view.view_name, utils.OrderedDict())[view.view_id] = view
-
-        self._sections_path.setdefault(utils.nameToPath(view.view_section), {}) \
-                           .setdefault(utils.nameToPath(view.view_name), utils.OrderedDict())[utils.nameToPath(view.view_id)] = view.view_path
 
     def loadViews(self):
         for view_class in sorted(pluginmanager.PluginManager("prewikka.views"), key=operator.attrgetter("view_order")):
@@ -491,8 +472,4 @@ class ViewManager:
     def __init__(self):
         self._views = {}
 
-        self._sections = utils.OrderedDict((k, utils.OrderedDict()) for k in env.config.section_order.keys())
-        self._sections_path = {}
-
         env.hookmgr.declare("HOOK_VIEW_LOAD", multi=True)
-        env.hookmgr.declare("HOOK_MENU_LOAD", multi=True)

commit 91c3acdea3681f07b7f176b90d937f318af8f781

[#1210] Fix plugin initialization
Fixes a bug in plugin initialization introduced
in commit 1eedcbf790dab64253713167c5a9795de912e283.

Change-Id: Idbf63ff72186fe36b3e14cea5102eda1d4971b0b

prewikka/pluginmanager.py
@@ -175,9 +175,10 @@ class PluginManager:
 
             try:
                 if issubclass(plugin_class, PluginPreload):
+                    # Get sections from all views before testing plugin database version
                     [self._handle_section(x) for x in plugin_class.plugin_classes]
                     self._handle_attributes(plugin_class, autoupdate)
-                    [self._addPlugin(x, autoupdate) for x in plugin_class.plugin_classes]
+                    [self._addPlugin(x, autoupdate) for x in plugin_class().plugin_classes]
                 else:
                     self._handle_section(plugin_class)
                     self._addPlugin(plugin_class, autoupdate, name=name)