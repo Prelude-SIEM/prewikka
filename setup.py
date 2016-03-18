0e550c1d6004e9630bed61b6e565974769f7e386
Antoine LUONG <antoine.luong@c-s.fr>
2016-03-18 11:29:19 +0100

    Update NEWS, bump version
    Change-Id: I7f090baac761ba0a8aeff6e2ad0cbbd8be72720d

--- setup.py
+++ setup.py
@@ -39,8 +39,8 @@ from distutils.command.sdist import sdist
 from distutils.errors import *
 from distutils import util
 
-LIBPRELUDE_REQUIRED_VERSION = "1.3.0beta1"
-LIBPRELUDEDB_REQUIRED_VERSION = "1.3.0beta1"
+LIBPRELUDE_REQUIRED_VERSION = "1.3.0beta2"
+LIBPRELUDEDB_REQUIRED_VERSION = "1.3.0beta2"
 
 from distutils.dep_util import newer
 
