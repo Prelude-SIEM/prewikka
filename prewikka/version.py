f46352e2e5012022a53d7a97f0b3e356867d7008
Antoine LUONG <antoine.luong@c-s.fr>
2016-04-15 17:44:27 +0200

    Update NEWS, bump version
    Change-Id: Ieba6e9ed2b4a37b26b7b0eea59adb2070f4ac6f0

--- prewikka/version.py
+++ prewikka/version.py
@@ -1,6 +1,6 @@
-__version_info__ = (1, 3, "0rc3")
+__version_info__ = (3, 0, "0rc4")
 __version__ = ".".join(str(i) for i in __version_info__)
-__branch__ = "1.3"
+__branch__ = "3.0"
 __author__ = "Prelude Team"
 __license__ = "GPL"
 __copyright__ = "CSSI"
--- setup.py
+++ setup.py
@@ -39,8 +39,8 @@ from distutils.command.sdist import sdist
 from distutils.errors import *
 from distutils import util
 
-LIBPRELUDE_REQUIRED_VERSION = "1.3.0rc3"
-LIBPRELUDEDB_REQUIRED_VERSION = "1.3.0rc3"
+LIBPRELUDE_REQUIRED_VERSION = "3.0.0rc4"
+LIBPRELUDEDB_REQUIRED_VERSION = "3.0.0rc4"
 
 from distutils.dep_util import newer
 
