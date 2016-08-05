b4a76399ae786328f8f87989b1868a3189c578e0
Antoine LUONG <antoine.luong@c-s.fr>
2016-08-05 18:40:52 +0200

    Update NEWS, bump version
    Change-Id: If502ee98d24dca2b07bb22a3a4fa01e0a8a6847e

--- prewikka/version.py
+++ prewikka/version.py
@@ -1,6 +1,6 @@
-__version_info__ = (3, 0, "0")
+__version_info__ = (3, 1, "0rc1")
 __version__ = ".".join(str(i) for i in __version_info__)
-__branch__ = "3.0"
+__branch__ = "3.1"
 __author__ = "Prelude Team"
 __license__ = "GPL"
 __copyright__ = "CSSI"
