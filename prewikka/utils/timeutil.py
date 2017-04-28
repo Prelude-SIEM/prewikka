48c40b948bc5e7ad2c35cef20d826a84f33c1c34
Antoine LUONG <antoine.luong@c-s.fr>
2017-04-28 18:14:39 +0200

    [#5341] Add time-related utility functions
    Change-Id: Id498f2176bdc295e9f05033c7fc7f4b27fc36b60

--- prewikka/utils/timeutil.py
+++ prewikka/utils/timeutil.py
@@ -2,10 +2,23 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import calendar
 
+from datetime import datetime
 from dateutil import parser
 from dateutil.tz import tzlocal, tzoffset, tzutc
 from pytz import timezone
 
 
+def now():
+    return datetime.now(env.request.user.timezone)
+
+
+def utcnow():
+    return datetime.now(timezone("UTC"))
+
+
 def get_timestamp_from_string(s):
     return calendar.timegm(parser.parse(s).timetuple()) if s else None
+
+
+def get_timestamp_from_datetime(dt):
+    return calendar.timegm(dt.utctimetuple())
