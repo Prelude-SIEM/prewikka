d8fed4831899e6944c1ac2086d215e3ecf0a5936
Antoine LUONG <antoine.luong@c-s.fr>
2017-05-03 11:22:06 +0200

    [#rel2492] Reload grids after deleting rows
    Change-Id: I6a04ce31eed6eabb0e648c0a3fd0e22965371ca4

--- prewikka/htdocs/js/commonlisting.js
+++ prewikka/htdocs/js/commonlisting.js
@@ -105,7 +105,7 @@ function CommonListing(elem, text, options) {
                 for ( var i = rows.length - 1; i >= 0; i-- )
                     grid.delRowData(rows[i]);
 
-                update_buttons_state(0);
+                grid.trigger("reloadGrid", [{current: true}]);
             }
         });
     }
