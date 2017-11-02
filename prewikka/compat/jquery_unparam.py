d7578eccdafbd3accc1645ceacdb7cd6b484c0ed
Yoann Vandoorselaere <yoannv@gmail.com>
2017-11-02 11:06:40 +0100

    [#1011] jQuery unparam fixes
    - Match parse_qsl() behavior with empty key
    - Return unicode
    - Unquote key and value at the right moment
    
    Change-Id: I1759a11a5d8666b5dac0a824bd69ba36a34fd123

--- prewikka/compat/jquery_unparam.py
+++ prewikka/compat/jquery_unparam.py
@@ -2,21 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import re
+import sys
 
 try:
     from urllib import unquote_plus
 except ImportError:
     from urllib.parse import unquote_plus
 
+if sys.version_info >= (3, 0):
+    Py3 = True
+else:
+    Py3 = False
 
-def parse_key_pair(keyval):
+
+def parse_key_pair(keyval, unquote=False):
     keyval_splitted = keyval.split('=', 1)
     if len(keyval_splitted) == 1:
         key, val = keyval_splitted[0], ''
     else:
         key, val = keyval_splitted
-    if key == '':
+
+    if key == '' or val == '':
         return {}
+
+    if unquote:
+        key = unquote_plus(key)
+        val = unquote_plus(val)
+
+    if not Py3:
+        key = key.decode("utf8")
+        val = val.decode("utf8")
 
     groups = re.findall(r"\[.*?\]", key)
     groups_joined = ''.join(groups)
@@ -27,12 +42,12 @@ def parse_key_pair(keyval):
                 val = [val]
             else:
                 val = {group[1:-1]: val}
+
     return {key: val}
 
 
 def merge_two_structs(s1, s2):
-    if isinstance(s1, list) and \
-       isinstance(s2, list):
+    if isinstance(s1, list) and isinstance(s2, list):
         return s1 + s2
 
     if isinstance(s1, dict) and \
@@ -52,21 +67,23 @@ def merge_two_structs(s1, s2):
 def merge_structs(structs):
     if len(structs) == 0:
         return None
+
     if len(structs) == 1:
         return structs[0]
+
     first, rest = structs[0], structs[1:]
     return merge_two_structs(first, merge_structs(rest))
 
 
-def jquery_unparam_unquoted(jquery_params):
+def _unparam(jquery_params, unquote=True):
     pair_strings = jquery_params.split('&')
-    key_pairs = [parse_key_pair(x) for x in pair_strings]
+    key_pairs = [parse_key_pair(x, unquote) for x in pair_strings]
     return merge_structs(key_pairs)
 
 
+def jquery_unparam_unquoted(jquery_params):
+    return _unparam(jquery_params, unquote=False)
+
+
 def jquery_unparam(jquery_params):
-    return jquery_unparam_unquoted(unquote_plus(jquery_params))
-
-
-if __name__ == '__main__':
-    pass
+    return _unparam(jquery_params, unquote=True)
