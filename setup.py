485adcb3763af0a6144803a1cc69e9eeb6c9bb24
Antoine LUONG <antoine.luong@c-s.fr>
2018-05-18 11:47:57 +0200

    Update NEWS, bump version
    Change-Id: I42379661d4456a29cc3da226fc00109c02a1d40a

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ class build_custom(Command):
 
 
 setup(name="prewikka",
-      version="4.1.9",
+      version="4.1.10",
       maintainer = "Prelude Team",
       maintainer_email = "support.prelude@c-s.fr",
       url = "http://www.prelude-siem.com",
