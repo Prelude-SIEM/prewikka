9954484cb4d003b95111abab5aad5df6f7d4fd6e
Antoine LUONG <antoine.luong@c-s.fr>
2019-02-01 17:03:55 +0100

    [#3417] Fix order of additional data in DataSearch
    Change-Id: I63dad32372ce726aee31e7e6ea7cf889163d7adf

--- prewikka/utils/__init__.py
+++ prewikka/utils/__init__.py
@@ -6,7 +6,7 @@ from . import html
 from . import timeutil
 from .cache import memoize, memoize_property, request_memoize, request_memoize_property
 from .misc import (
-    AttrObj, get_analyzer_status_from_latest_heartbeat, protocol_number_to_name, nameToPath,
+    AttrObj, get_analyzer_status_from_latest_heartbeat, protocol_number_to_name, nameToPath, path_sort_key,
     find_unescaped_characters, split_unescaped_characters, soundex, hexdump, deprecated, CachingIterator
 )
 from .url import mkdownload, iri2uri, urlencode
