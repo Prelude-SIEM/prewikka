b412519764df4baf7138bcae3ae2e3c5a637ea51
Antoine LUONG <antoine.luong@c-s.fr>
2019-06-12 19:31:44 +0200

    Update NEWS, bump version
    Change-Id: Ie21c4207b4a874619171d7a1e37097cb0e812c8e

--- setup.py
+++ setup.py
@@ -215,7 +215,7 @@ class PrewikkaCoverage(Command):
 
 setup(
     name="prewikka",
-    version="5.0.4",
+    version="5.0.5",
     maintainer="Prelude Team",
     maintainer_email="support.prelude@c-s.fr",
     url="http://www.prelude-siem.com",
